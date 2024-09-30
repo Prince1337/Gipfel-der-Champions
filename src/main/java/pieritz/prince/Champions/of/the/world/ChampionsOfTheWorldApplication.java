b50149c7f4f78b49c58df5d9062897786b703967
Prince1337 <prince.pieritz@gmail.com>
2024-09-30 12:08:09 +0200

    git init

---
+++ src/main/java/pieritz/prince/Champions/of/the/world/ChampionsOfTheWorldApplication.java
@@ -0,0 +1,13 @@
+package pieritz.prince.Champions.of.the.world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampionsOfTheWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChampionsOfTheWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pieritz/prince/Champions/of/the/world/ChampionsOfTheWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package pieritz.prince.Champions.of.the.world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampionsOfTheWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
