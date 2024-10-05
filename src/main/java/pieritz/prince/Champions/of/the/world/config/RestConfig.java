b748a8b0526de8d1d44b10f1abdd6fa36b73744b
Prince1337 <prince.pieritz@gmail.com>
2024-10-05 19:15:31 +0200

    feat: secure endpoints

---
+++ src/main/java/pieritz/prince/Champions/of/the/world/config/RestConfig.java
@@ -0,0 +1,26 @@
+package pieritz.prince.Champions.of.the.world.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import pieritz.prince.Champions.of.the.world.domain.F1WorldChampion;
+
+@Configuration
+public class RestConfig implements RepositoryRestConfigurer {
+
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+
+        config.getExposureConfiguration()
+                .forDomainType(F1WorldChampion.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods
+                        .disable(HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE)
+                ).withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE));
+
+    }
+}
+
